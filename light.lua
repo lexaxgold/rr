4f1dab97d2ccb79222fe414c80b1514405069e07
lexaxgold <60924053+lexaxgold@users.noreply.github.com>
2022-12-28 00:19:38 +0300

    Update light.lua

--- light.lua
+++ light.lua
@@ -58,3 +58,42 @@ end)
 SCTAB:NewButton("[CTRL] Click Teleport [KICK RISK]", "Click CTRL to teleport", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/lexaxgold/rr/main/ClickTeleport.txt", true))()
 end)
+
+SCTAB:NewButton("ESP", "See everyone", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua',true))()
+end)
+
+SCTAB:NewButton("Enable Reset Charachter", "Enables Reset Charachter", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lexaxgold/rr/main/Enable%20Reset%20Character.txt", true))()
+end)
+
+SCTAB:NewButton("Dex Explorer", "Explorer Menu", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lexaxgold/rr/main/Dex%20Explorer%20V2.txt", true))()
+end)
+
+SCTAB:NewButton("FPS Aimlock", "Work's only on FPS games", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lexaxgold/rr/main/FPS%20Aimlock.lua", true))()
+end)
+
+SCTAB:NewButton("Headless", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lexaxgold/rr/main/Headless.txt", true))()
+end)
+
+SCTAB:NewButton("Hitbox Extender", "Makes hitboxes bigger.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lexaxgold/rr/main/Hitbox%20Extender.txt", true))()
+end)
+
+SCTAB:NewButton("Infinite Donuts", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lexaxgold/rr/main/Infinite%20Donuts.lua", true))()
+end)
+
+SCTAB:NewButton("Infinite Jump [Bad one]", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lexaxgold/rr/main/Infinite%20Jump.txt", true))()
+end)
+
+-- Updates Tab
+local Tab = Window:NewTab("Updates")
+local UPDTAB = Tab:NewSection("- 28/12/2022 -")
+
+UPDTAB:NewLabel("- Few scripts are added to [Scripts] panel.")
+UPDTAB:NewLabel("- AutoFarm added to [ - Booga Booga [GUI] - ].")
